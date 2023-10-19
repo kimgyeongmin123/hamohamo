00e631989f5b6cf3831cbb1e4c0689af8673155b
kimgyeongmin123 <orange761943@naver.com>
2023-10-20 00:48:43 +0900

    파일여러개올리기가능

---
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,53 @@
+package com.example.demo.config;
+
+
+import com.example.demo.domain.repository.BoardRepository;
+//import com.example.demo.interceptor.BoardInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60 * 60 * 24 * 365);
+
+        /* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/").setCachePeriod(60 * 60 * 24 * 365);
+
+
+        /* '/font/**'로 호출하는 자원은 '/static/font/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60 * 60 * 24 * 365);
+
+        //이미지 경로 추가(윈도우 : c:\\hamohamo를 기본경로로 둔다)
+        registry.addResourceHandler("/resources/hamohamo/**").addResourceLocations("file:c:\\hamohamo\\").setCachePeriod(60 * 60 * 24 * 365);
+
+
+    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        registry.addInterceptor(new BoardInterceptor(boardRepository))
+//                .addPathPatterns("/board/update")
+//                .excludePathPatterns("/resources/**");
+//    }
+
+
+
+
+}
