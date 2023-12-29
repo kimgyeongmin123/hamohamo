65e6309d4da838ed3b57920a341474b6be19715b
kimgyeongmin123 <orange761943@naver.com>
2023-12-29 14:47:29 +0900

    일단

--- src/main/java/com/example/demo/domain/repository/MessageRepository.java
+++ src/main/java/com/example/demo/domain/repository/MessageRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    @Query("SELECT m FROM Message m WHERE (m.send_nick = :nickname AND m.recv_nick = :sendNickname) ORDER BY m.sendTime ASC")
+    @Query("SELECT m FROM Message m WHERE (m.send_nick = :nickname AND m.recv_nick = :sendNickname) OR (m.send_nick = :sendNickname AND m.recv_nick = :nickname) ORDER BY m.sendTime ASC")
     List<Message> getMessageBySendEmail(@Param("nickname") String nickname, @Param("sendNickname") String sendNickname);
 
 }
